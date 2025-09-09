67615948aa916081d1ab06833916d599074bf22d
Sascha-netizen <sascha.r.klement@gmail.com>
2025-09-09 11:49:48 +0200

    Fix 'this' keyword in DOM function

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     for (let button of buttons) {
         button.addEventListener("click", function() {
-            if (this.getAttribute("data-type") === "submit") {
+            if (button.getAttribute("data-type") === "submit") {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
